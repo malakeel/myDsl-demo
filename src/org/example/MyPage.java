aa5f3ce21ce1fbb2ead2145fb4d6cd44b82b3bc7
Mansour Al Akeel <mansour.alakeel@gmail.com>
2016-06-18 20:36:55 -0400

    init

---
+++ src/org/example/MyPage.java
@@ -0,0 +1,16 @@
+package org.example ;
+import org.example.IAction;
+
+public class MyPage {
+
+	public static IAction element = null;
+
+	public static IAction text = null;
+
+	public static String invalide = "";
+
+	public static IAction getValues(int i) {
+		return null;
+	}
+
+}
---
+++ src/org/example/testing/another/MyOtherPage.java
@@ -0,0 +1,16 @@
+package org.example.testing.another ;
+import org.example.IAction;
+
+public class MyOtherPage {
+
+	public static IAction element = null;
+
+	public static IAction text = null;
+
+	public static String invalide = "";
+
+	public static IAction getValues(int i) {
+		return null;
+	}
+
+}
